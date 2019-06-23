b628eb0e02316e223707bb4d4308fb3664c8eb30
Rodrigo Klaes <rodrigoklaes@gmail.com>
2019-06-23 20:32:59 -0300

    Finished

---
+++ app/Controllers/Http/ClienteController.js
@@ -0,0 +1,81 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Cliente = use("App/Models/Cliente");
+
+/**
+ * Resourceful controller for interacting with clientes
+ */
+class ClienteController {
+  /**
+   * Show a list of all clientes.
+   * GET clientes
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+
+    const clientes = await Cliente.all()
+
+    return clientes
+
+  }
+
+  /**
+   * Create/save a new cliente.
+   * POST clientes
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    const data = request.only(['nome', 'email']);
+
+    const cliente = await Cliente.create(data)
+
+    return cliente
+  }
+
+  /**
+   * Update cliente details.
+   * PUT or PATCH clientes/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    const cliente = await Cliente.findOrFail(params.id)
+    const data = request.only(['nome', 'email'])
+    cliente.merge(data)
+
+    await cliente.save();
+
+    return cliente
+  }
+
+  /**
+   * Delete a cliente with id.
+   * DELETE clientes/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+    const cliente = await Cliente.findOrFail(params.id)
+
+    await cliente.delete()
+
+    return
+  }
+}
+
+module.exports = ClienteController
